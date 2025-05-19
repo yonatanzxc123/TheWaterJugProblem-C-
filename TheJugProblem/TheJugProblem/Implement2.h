89473e639d074a3fc706dc2b0772b218f7192529
Idan Yossifov <idanyossifov@gmail.com>
2025-05-19 13:02:11 +0300

    NO COMMENTS!!!!!!!!!!

--- TheJugProblem/TheJugProblem/Implement2.h
+++ TheJugProblem/TheJugProblem/Implement2.h
@@ -8,6 +8,7 @@ class Implement2
 {
 public:
     Solution solveBFS_onTheFly(int L, int S, int W);
+
     std::vector<std::pair<int, int>> CalculateAdjList(const std::pair<int, int>& u, int L, int S, const std::unordered_set<std::pair<int, int>, PairHash>& visited);
 };
 
